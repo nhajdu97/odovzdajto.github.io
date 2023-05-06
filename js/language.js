25e0ae4699596e8d00ef15b9bc3432c4c218ad91
nhajdu97 <132741747+nhajdu97@users.noreply.github.com>
2023-05-06 12:01:44 +0200

    Add files via upload

--- js/language.js
+++ js/language.js
@@ -1,1 +1,12 @@
-
+function switchLanguage(langCode) {
+  var currentUrl = window.location.href;
+  if (langCode === 'sk') {
+    window.location.href = currentUrl.replace('/en/home.html', '/domov.html')
+                                       .replace('/en/registration.html', '/registracia.html')
+                                       .replace('/en/login.html', '/prihlasenie.html');
+  } else if (langCode === 'en') {
+    window.location.href = currentUrl.replace('/domov.html', '/en/home.html')
+                                       .replace('/registracia.html', '/en/registration.html')
+                                       .replace('/prihlasenie.html', '/en/login.html');
+  }
+}
