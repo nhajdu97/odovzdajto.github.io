73068ac3089a442357fb6f2fdd72fd93d47a8170
nhajdu97 <132741747+nhajdu97@users.noreply.github.com>
2023-05-10 20:40:25 +0200

    Add files via upload

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -1,16 +1,14 @@
-// Show or hide the link based on the scroll position
-  window.addEventListener('scroll', function() {
-    var scrollToTopLink = document.getElementById("scrolltop");
-    var scrollPosition = window.innerHeight + window.scrollY;
-    if (scrollPosition >= document.body.offsetHeight) {
-      scrollToTopLink.style.display = "block";
-    } else {
-      scrollToTopLink.style.display = "none";
-    }
-  });
-
-  // Scroll to the top of the page when the link is clicked
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
+window.onscroll = function() {
+	var e = document.getElementById("scrolltop");
+    if (!e) {
+    	e = document.createElement("a");
+        e.id = "scrolltop";
+        e.href = "#";
+        document.body.appendChild(e);
+	}
+	e.style.display = document.documentElement.scrollTop > 300 ? "block" : "none";
+    e.onclick = (ev) => {
+    	ev.preventDefault();
+        document.documentElement.scrollTop = 0;
+    };
+};
