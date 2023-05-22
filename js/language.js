9f35c53543372416e5b46067a9962cedfbdf7771
nhajdu97 <132741747+nhajdu97@users.noreply.github.com>
2023-05-22 20:37:19 +0200

    Add files via upload

--- js/language.js
+++ js/language.js
@@ -1,22 +1,24 @@
 function switchLanguage(langCode) {
   var currentUrl = window.location.href;
   if (langCode === 'sk') {
-    window.location.href = currentUrl.replace('/en/home.html', '/index.html')
+    window.location.href = currentUrl.replace('/en/home.html', '/domov.html')
                                        .replace('/en/registration.html', '/registracia.html')
                                        .replace('/en/login.html', '/prihlasenie.html')
                                        .replace('/en/pedagogue.html', '/system/pedagog.html')
                                        .replace('/en/student.html', '/system/student.html')
                                        .replace('/en/password.html', '/heslo.html')
                                        .replace('/en/overview.html', '/system/prehlad.html')
-                                       .replace('/en/assignments.html', '/system/zadania.html');
+                                       .replace('/en/assignments.html', '/system/zadania.html')
+                                       .replace('/en/profile.html', '/system/profil.html');
   } else if (langCode === 'en') {
-    window.location.href = currentUrl.replace('/index.html', '/en/home.html')
+    window.location.href = currentUrl.replace('/domov.html', '/en/home.html')
                                        .replace('/registracia.html', '/en/registration.html')
                                        .replace('/prihlasenie.html', '/en/login.html')
                                        .replace('/system/pedagog.html', '/en/pedagogue.html')
                                        .replace('/system/student.html', '/en/student.html')
                                        .replace('/heslo.html', '/en/password.html')
                                        .replace('/system/prehlad.html', '/en/overview.html')
-                                       .replace('/system/zadania.html', '/en/assignments.html');
+                                       .replace('/system/zadania.html', '/en/assignments.html')
+                                       .replace('/system/profil.html', '/en/profile.html');
   }
 }
