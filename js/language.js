59263aa888b70dd72b93990d3ea78a008e32d467
nhajdu97 <132741747+nhajdu97@users.noreply.github.com>
2023-05-22 20:40:09 +0200

    Update language.js

--- js/language.js
+++ js/language.js
@@ -1,7 +1,7 @@
 function switchLanguage(langCode) {
   var currentUrl = window.location.href;
   if (langCode === 'sk') {
-    window.location.href = currentUrl.replace('/en/home.html', '/domov.html')
+    window.location.href = currentUrl.replace('/en/home.html', '/index.html')
                                        .replace('/en/registration.html', '/registracia.html')
                                        .replace('/en/login.html', '/prihlasenie.html')
                                        .replace('/en/pedagogue.html', '/system/pedagog.html')
@@ -11,7 +11,7 @@ function switchLanguage(langCode) {
                                        .replace('/en/assignments.html', '/system/zadania.html')
                                        .replace('/en/profile.html', '/system/profil.html');
   } else if (langCode === 'en') {
-    window.location.href = currentUrl.replace('/domov.html', '/en/home.html')
+    window.location.href = currentUrl.replace('/index.html', '/en/home.html')
                                        .replace('/registracia.html', '/en/registration.html')
                                        .replace('/prihlasenie.html', '/en/login.html')
                                        .replace('/system/pedagog.html', '/en/pedagogue.html')
